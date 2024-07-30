b3253515788777ded5ea59662e2c4f827fe547c8
pc03025 <rjw10003@polycube.co.kr>
2024-07-30 13:40:22 +0900

    수정

--- src/main/kotlin/com/example/kopringpractice/blog/entity/Wordcount.kt
+++ src/main/kotlin/com/example/kopringpractice/blog/entity/Wordcount.kt
@@ -1,5 +1,6 @@
 package com.example.kopringpractice.blog.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 
@@ -7,6 +8,6 @@ import jakarta.persistence.Id
 class Wordcount(
     @Id
     val word: String,
-
+    @Column(name = "count")
     val cnt: Int = 0
 )
